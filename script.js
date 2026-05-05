1392496b2f0a0f5a779ff715554d9aa51a2b1159
Celso Bitello <celsokaiiki@gmail.com>
2026-05-04 22:38:58 -0300

    G7 - Trabalhando com Json

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
 var fields = document.querySelectorAll("#form-user-create [name]"); //puxa todos os dados do campo determinado do html
+var user = {};
 
 fields.forEach(function(field, index){
 
     if (field.name == "gender" && field.checked) {
 
-        console.log("SIM", field)
+        user.gender
 
     } else {
 
-        console.log("Não")
-    }
+        user[field.name] = field.value; //atribuí o nome do usuário
+        
 
+
+    }
 });
+
+console.log(user)
