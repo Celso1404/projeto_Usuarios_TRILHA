89b40dade66fe91219da2371506ee21ac9167787
Celso Bitello <celsokaiiki@gmail.com>
2026-05-06 00:44:00 -0300

    G8 - Trabalhando com eventos no Javascript

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 var fields = document.querySelectorAll("#form-user-create [name]"); //puxa todos os dados do campo determinado do html
 var user = {};
 
-fields.forEach(function(field, index){
+
+document.getElementById("form-user-create").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    fields.forEach(function(field, index){
 
     if (field.name == "gender" && field.checked) {
 
-        user.gender
+        user[field.name] = field.value;
 
     } else {
 
         user[field.name] = field.value; //atribuí o nome do usuário
         
-
-
     }
 });
 
-console.log(user)
+console.log(user);
+
+});
