3a66a9903b5d8d92c7383ca0f691d9328b61a839
Celso Bitello <celsokaiiki@gmail.com>
2026-05-07 01:52:27 -0300

    G19 - Atualizando Estatísticas - Usando dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,6 +21,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
                (content)=> {
 
@@ -106,7 +108,7 @@ class UserController {
     if(!isValid) {
 
         return false;
-        
+
     }
 
     return new User(user.name, 
@@ -123,6 +125,8 @@ class UserController {
 
     let tr = document.createElement('tr');
 
+    tr.dataset.user = JSON.stringify(dataUser); //coloca os dados do objeto em uma string JSON
+
     tr.innerHTML = `
                     <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                     <td>${dataUser.name}</td>
@@ -136,6 +140,27 @@ class UserController {
                   ;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
                     
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=> {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
     }
 }
