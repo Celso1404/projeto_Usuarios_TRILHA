c5196d8d8f6e20b3803215a29a62072d910fc317
Celso Bitello <celsokaiiki@gmail.com>
2026-05-08 01:31:15 -0300

    G23 - Trabalhando com Edição de Dados - Editando valores com Radio e Checkbox.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -159,8 +159,22 @@ class UserController {
 
                 if (field) {
 
-                    if(field.type == 'file') continue;
-                    field.value = json[name];
+                    switch (field.type) {
+                        case 'file':
+                            continue;
+                            break;
+
+                        case 'radio':
+                            field = form.querySelector("[name=" + name.replace("_", "") + "][value="+json[name]+ "]");
+                            field.checked = true;
+                            break;
+
+                            case 'checkbox':
+                                field.checked = json[name];
+                                break;
+                        default:
+                            field.value = json[name];        
+                    }
                 }
             }
 
