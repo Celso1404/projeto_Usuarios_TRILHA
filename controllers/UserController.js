6da14caa6b67531816609d86a54d07299fea4a3e
Celso <celsokaiiki@gmail.com>
2026-05-09 01:19:10 -0300

    G29 - Refactoring - Otimizando o Template

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -42,21 +42,10 @@ class UserController {
                     result._photo = content;
                  }   
 
-                tr.dataset.user = JSON.stringify(result);
+            let user = new User();     
 
-                 tr.innerHTML = `
-                    <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                    <td>${result._name}</td>
-                    <td>${result._email}</td>
-                    <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-                    <td>${Utils.dateFormat(result._register)}</td>
-                    <td>
-                      <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>`
-                  ;  
-
-            this.addEventsTR(tr);
+            user.loadFromJSON(result);
+            this.getTr(user, tr);
             this.updateCount();
             this.formUpdateEl.reset();
             btn.disabled = false;
@@ -207,13 +196,22 @@ class UserController {
         localStorage.setItem("users", JSON.stringify(users));
     }
 
-    addLine(dataUser) {
+    addLine(dataUser) { 
 
-    let tr = document.createElement('tr');
+    let tr = this.getTr(dataUser);
 
-    tr.dataset.user = JSON.stringify(dataUser); //coloca os dados do objeto em uma string JSON
+    this.tableEl.appendChild(tr);
 
-    tr.innerHTML = `
+    this.updateCount();   
+}
+
+    getTr(dataUser, tr = null) {
+
+        if (tr === null) tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
                     <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                     <td>${dataUser.name}</td>
                     <td>${dataUser.email}</td>
@@ -225,12 +223,9 @@ class UserController {
                     </td>`
                   ;
 
-    this.addEventsTR(tr);    
-
-    this.tableEl.appendChild(tr);
-
-    this.updateCount();   
-}
+            this.addEventsTR(tr);       
+            return tr;  
+    }
 
     addEventsTR(tr) {
 
